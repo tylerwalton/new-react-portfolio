6e71c83d6b8ea9088bb5bf3877a1ede05034f2c7
Tyler Walton <tylerwalton300@gmail.com>
2023-03-17 20:54:47 -0400

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Resume from './components/Resume';
+import Contact from './components/Contact';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,7 +3,7 @@ import React from "react";
 const Resume = () => {
   return (
     <div>
-      <h2> This is a resumer</h2>
+      <h2> This is a resume</h2>
     </div>
   );
 };
